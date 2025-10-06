ee76c4073b657548e24c2a82170ca68322d18941
ilongen <iago.mendonca_2005@hotmail.com>
2025-10-06 17:27:05 -0300

    escopo da conexão feita, agora falta validar.

--- app/src/dbClasses/connDatabase.java
+++ app/src/dbClasses/connDatabase.java
@@ -1,4 +1,21 @@
 package dbClasses;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+
 public class connDatabase {
+    private String URL = "jdbc:mysql://localhost:1433/UniversidadeTexas";
+    private String user = "";
+    private String password = "";
+
+    public connDatabase(){
+        try {
+            Connection conn = DriverManager.getConnection(URL,user,password);
+            System.out.println("Conectado com sucesso!");
+            conn.close();
+        } catch (SQLException e){
+            System.out.println("Erro ao conectar com o Banco de Dados!"+ e.getMessage());
+        }
+    }
 }
